a33b4e754e6472d8b0703f7542e1f4b6ad1de1b4
lusiferli <lusiferli@tencent.com>
2019-06-24 20:35:34 +0800

    取值

--- args/args_command.go
+++ args/args_command.go
@@ -1,13 +1,25 @@
 package main
 
+import "strings"
+
 type Command struct {
 	Flag  string
 	Value string
 }
 
 func CreateCommand(commandStr string) Command {
+	items := strings.Split(commandStr, " ")
+	var flag string
+	var value string
 	command := Command{}
-	command.Flag = ""
-	command.Value = ""
+	if len(items) > 2 || len(items) == 0 {
+		return command
+	}
+	flag = items[0]
+	if len(items) == 1 {
+		value = items[1]
+	}
+	command.Flag = flag
+	command.Value = value
 	return command
 }
--- args/args_command_test.go
+++ args/args_command_test.go
@@ -7,7 +7,11 @@ import (
 func Test_parse_command(t *testing.T) {
 	commandStr := "-f 12"
 	var command = CreateCommand(commandStr)
-	if command != (Command{}) {
+	if command == (Command{}) {
+		t.Fail()
+	}
+
+	if command.Flag != "-f" {
 		t.Fail()
 	}
 }
